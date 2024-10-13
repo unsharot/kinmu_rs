de295d672c3de723be2a72c7624e195bbe281259
unsharot <U_N_Sharot@yahoo.co.jp>
2024-10-14 01:54:56 +0900

    update1を修正

--- src/update.rs
+++ src/update.rs
@@ -10,6 +10,7 @@ use rand::Rng;
 
 
 pub fn gen_update_func<'a>(text: &str, hp: &'a HyouProp, hst: &'a HyouST) -> Box<dyn FnMut(&Hyou) -> Hyou + 'a> {
+    println!("{}", text);
     match text {
         "update1" => Box::new(move |h| update_randomly1(hp, hst, h)),
         "update4" => Box::new(move |h| update_randomly4(hp, hst, h)),
@@ -38,7 +39,7 @@ fn update_randomly1(hp: &HyouProp, hst: &HyouST, h: &Hyou) -> Hyou {
     let rx: usize = rng.gen_range(0..hp.worker_count);
     let ry: usize = rng.gen_range(0..hp.day_count);
     if hst[rx][ry] != WakuST::Absolute {
-        newh[rx][ry] = [Waku::N, Waku::K, Waku::I, Waku::A, Waku::O, Waku::H][rng.gen_range(0..3)];
+        newh[rx][ry] = [Waku::N, Waku::K, Waku::I, Waku::A, Waku::O, Waku::H][rng.gen_range(0..6)];
         newh
     } else {
         update_randomly1(&hp, &hst, &h)
