49f51bc5a52db87e515214841011eab9bb820506
unsharot <U_N_Sharot@yahoo.co.jp>
2024-12-07 00:40:48 +0900

    読み込むconfigファイルを指定できるよう変更

--- src/main.rs
+++ src/main.rs
@@ -3,19 +3,24 @@ use kinmu::io::{display, reader};
 use kinmu::kinmu_lib::{check, fill, score, update};
 
 use std::time::Instant;
+use std::env;
 
 fn main() {
-    match generate_schedules() {
+    let args: Vec<String> = env::args().collect();
+    let main_file_path = if args.len() >= 2 {
+        &args[1]
+    } else {
+        "config/config.yaml"
+    };
+    match generate_schedules(main_file_path) {
         Ok(_) => {}
         Err(e) => println!("{}", e),
     }
 }
 
-fn generate_schedules() -> Result<(), String> {
-    let config_path = "config/config.yaml".to_string();
-
+fn generate_schedules(config_path: &str) -> Result<(), String> {
     let schedule_config_paths: Vec<String> =
-        reader::load_main_config(&config_path).map_err(|e| {
+        reader::load_main_config(config_path).map_err(|e| {
             format!(
                 "[エラー] メインconfigの読み込みに失敗しました\n対象ファイル: {}\n理由: {}",
                 config_path, e
