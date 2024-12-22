78a7b1e39cc48fd7ef745b87375c3300d1846f6e
unsharot <U_N_Sharot@yahoo.co.jp>
2024-12-22 16:02:08 +0900

    Add code for parallel

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use kinmu::io::{display, reader};
 use kinmu::kinmu_lib::{check, fill, score, update};
 
 use std::env;
+use std::thread;
 use std::time::Instant;
 
 fn main() {
@@ -12,7 +13,8 @@ fn main() {
     } else {
         "config/config.yaml"
     };
-    match generate_schedules(main_file_path) {
+    // match generate_schedules(main_file_path) {
+    match generate_schedules2(main_file_path) {
         Ok(_) => {}
         Err(e) => println!("{}", e),
     }
@@ -121,6 +123,105 @@ fn generate_schedule(p: &str) -> Result<(), String> {
     );
 
     println!();
+
+    Ok(())
+}
+
+fn generate_schedules2(config_path: &str) -> Result<(), String> {
+    let schedule_config_paths: Vec<String> =
+        reader::load_main_config(config_path).map_err(|e| {
+            format!(
+                "[エラー] メインconfigの読み込みに失敗しました\n対象ファイル: {}\n理由: {}",
+                config_path, e
+            )
+        })?;
+
+    for path in schedule_config_paths {
+        generate_schedule(&path)?;
+    }
+
+    Ok(())
+}
+
+fn generate_schedule2(p: &str) -> Result<(), String> {
+    let (mut schedule_prop, ac_paths, mut fc) = reader::load_schedule_config(p).map_err(|e| {
+        format!(
+            "[エラー] 勤務表configの読み込みに失敗しました\n対象ファイル: {}\n理由: {}",
+            p, e
+        )
+    })?;
+
+    let mut annealing_configs = vec![];
+    for ac_path in ac_paths {
+        annealing_configs.push(reader::load_annealing_config(&ac_path).map_err(|e| {
+            format!(
+                "[エラー] 焼きなましconfigの読み込みに失敗しました\n対象ファイル: {}\n理由: {}",
+                ac_path, e
+            )
+        })?);
+    }
+
+    let n = 6;
+
+    let mut hs = vec![];
+    for i in 0..n {
+        let schedule_prop = schedule_prop.clone();
+        let annealing_configs = annealing_configs.clone();
+        let mut fc = fc.clone();
+        hs.push(thread::spawn(move || {
+            let mut model = fill::run(&mut fc, &schedule_prop)?;
+
+            let mut score;
+            for ac in annealing_configs {
+                let mut rng = ac.rng;
+                score = score::assess_score(&mut ac.score_props, &schedule_prop, &model);
+                (score, model) = annealing::annealing(
+                    score,
+                    &model,
+                    ac.step,
+                    update::gen_update_func(&ac.update_func, &schedule_prop)?,
+                    |m| score::assess_score(&mut ac.score_props, &schedule_prop, m),
+                    ac.max_temp,
+                    ac.min_temp,
+                    annealing::basic_temp_func,
+                    annealing::basic_prob_func,
+                    &mut rng,
+                );
+            }
+
+            Ok(model)
+        }))
+    }
+
+    for (i, h) in hs.iter().enumerate() {
+        let model = h.join().unwrap()?;
+
+        println!("thread: {}", i);
+
+        println!();
+
+        let score = score::assess_score(
+            &mut schedule_prop.score_props.clone(),
+            &mut schedule_prop,
+            &model,
+        );
+
+        println!("{}", score);
+        display::print_schedule(&schedule_prop, &model);
+
+        println!();
+
+        println!(
+            "{}",
+            score::show_score(
+                &mut schedule_prop.score_props.clone(),
+                &mut schedule_prop,
+                &model
+            )
+        );
+
+        println!();
+    }
 
     Ok(())
 }
