e0e8a7401683ccc0d42a24d9135c356fd0c0c6ff
unsharot <U_N_Sharot@yahoo.co.jp>
2025-01-01 21:35:43 +0900

    Refactor reader/mod.rs

--- src/io/reader/mod.rs
+++ src/io/reader/mod.rs
@@ -5,13 +5,15 @@ mod common;
 mod toml_reader;
 mod type_reader;
 
-use toml_reader::MainConfig;
+use toml_reader::{MainConfig, ScheduleConfig};
 use type_reader::*;
 
 use crate::kinmu_lib::types::{
-    AnnealingConfig, Days, FillConfig, Schedule, ScheduleProp, ScheduleState, ScoreProp, Shift,
-    ShiftState, Staff, StaffAttributeNameIndexMap,
+    AnnealingConfig, DayAttributeName, Days, FillConfig, Schedule, ScheduleProp, ScheduleState,
+    ScoreProp, Shift, ShiftState, Staff, StaffAttributeNameIndexMap,
 };
+
+use std::collections::HashMap;
 
 type FilePath = String;
 
@@ -25,17 +27,7 @@ pub fn load_schedule_config(
 ) -> Result<(ScheduleProp, Vec<FilePath>, FillConfig), String> {
     let config = toml_reader::read_schedule_config(path)?;
 
-    let schedule: Vec<Vec<Shift>> = {
-        let mut ans: Schedule = Vec::new();
-        for s in config.day.requested_schedule {
-            let mut row = Vec::new();
-            for c in s.chars() {
-                row.push(c.to_string().parse::<Shift>().map_err(|e| e.to_string())?);
-            }
-            ans.push(row);
-        }
-        Ok::<Vec<Vec<Shift>>, String>(ans)
-    }?;
+    let schedule = make_schedule(&config)?;
 
     let schedule_prop: ScheduleProp = ScheduleProp {
         staff_list: config
@@ -59,25 +51,8 @@ pub fn load_schedule_config(
         buffer: config.day.buffer_count,
         request: schedule.clone(),
         schedule_st: make_schedule_state(&schedule, config.day.buffer_count),
-        day_attributes: {
-            use std::collections::HashMap;
-            let mut ans = HashMap::new();
-            for att in config.day.attributes {
-                ans.insert(att.name, att.values);
-            }
-            ans
-        },
-        staff_attribute_map: {
-            use std::collections::HashMap;
-            let mut name_to_index = HashMap::new();
-            for (i, name) in config.staff.attributes.iter().enumerate() {
-                name_to_index.insert(name.to_string(), i);
-            }
-            StaffAttributeNameIndexMap {
-                names: config.staff.attributes,
-                name_to_index,
-            }
-        },
+        day_attributes: make_day_attributes(&config),
+        staff_attribute_map: make_staff_attribute_map(&config),
         score_props: config
             .result
             .score_functions
@@ -112,6 +87,37 @@ pub fn load_annealing_config(path: &str) -> Result<AnnealingConfig, String> {
     };
 
     Ok(ac)
+}
+
+fn make_schedule(config: &ScheduleConfig) -> Result<Schedule, String> {
+    let mut ans: Schedule = Vec::new();
+    for s in &config.day.requested_schedule {
+        let mut row = Vec::new();
+        for c in s.chars() {
+            row.push(c.to_string().parse::<Shift>().map_err(|e| e.to_string())?);
+        }
+        ans.push(row);
+    }
+    Ok(ans)
+}
+
+fn make_day_attributes(config: &ScheduleConfig) -> HashMap<DayAttributeName, Vec<isize>> {
+    let mut ans = HashMap::new();
+    for att in &config.day.attributes {
+        ans.insert(att.name.clone(), att.values.clone());
+    }
+    ans
+}
+
+fn make_staff_attribute_map(config: &ScheduleConfig) -> StaffAttributeNameIndexMap {
+    let mut name_to_index = HashMap::new();
+    for (i, name) in config.staff.attributes.iter().enumerate() {
+        name_to_index.insert(name.to_string(), i);
+    }
+    StaffAttributeNameIndexMap {
+        names: config.staff.attributes.clone(),
+        name_to_index,
+    }
 }
 
 fn make_schedule_state(schedule: &Schedule, buffer: usize) -> ScheduleState {
