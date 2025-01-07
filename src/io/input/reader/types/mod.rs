530b6e863dabca96fef80534f43f442cb112dc70
unsharot <U_N_Sharot@yahoo.co.jp>
2025-01-07 11:34:07 +0900

    Make `use` safer

--- src/io/input/reader/types/mod.rs
+++ src/io/input/reader/types/mod.rs
@@ -4,6 +4,6 @@ mod annealing_config;
 mod main_config;
 mod schedule_config;
 
-pub use annealing_config::*;
-pub use main_config::*;
-pub use schedule_config::*;
+pub use self::annealing_config::*;
+pub use self::main_config::*;
+pub use self::schedule_config::*;
