c2315b93f475953b45a115055262b4da1fe6d0be
unsharot <U_N_Sharot@yahoo.co.jp>
2025-01-08 13:41:52 +0900

    Improve interface of output module

--- src/io/output/mod.rs
+++ src/io/output/mod.rs
@@ -1,16 +1,9 @@
-//! CLI出力のためのモジュール
-//! 結果を受け取り、ユーザーに出力する
+//! 結果をユーザーに出力するモジュール
 
 mod display;
 
 use crate::kinmu_lib::types::Answer;
 
-pub fn show(ans: Answer) {
-    for (t, model) in ans.models.iter().enumerate() {
-        println!("thread: {}", t + 1);
-        display::print_model(&ans.schedule_prop, &model);
-    }
-    println!();
-    println!();
-    println!("total time: {:?}", ans.total_time);
+pub fn run(ans: Answer) {
+    display::print_answer(ans);
 }
