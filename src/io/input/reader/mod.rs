d552a5aa22e7189345ab53f593436537f0642e1d
unsharot <U_N_Sharot@yahoo.co.jp>
2025-01-08 11:24:18 +0900

    Improve `pub` by using `pub(super)`

--- src/io/input/reader/mod.rs
+++ src/io/input/reader/mod.rs
@@ -2,24 +2,24 @@
 
 use types::{RawAnnealingConfig, RawMainConfig, RawScheduleConfig};
 
-pub mod types;
+pub(super) mod types;
 
 use std::fs;
 use toml;
 
-pub fn read_main_config(path: &str) -> Result<RawMainConfig, String> {
+pub(super) fn read_main_config(path: &str) -> Result<RawMainConfig, String> {
     let contents = fs::read_to_string(path).map_err(|e| e.to_string())?;
     let config: RawMainConfig = toml::from_str(&contents).map_err(|e| e.to_string())?;
     Ok(config)
 }
 
-pub fn read_schedule_config(path: &str) -> Result<RawScheduleConfig, String> {
+pub(super) fn read_schedule_config(path: &str) -> Result<RawScheduleConfig, String> {
     let contents = fs::read_to_string(path).map_err(|e| e.to_string())?;
     let config: RawScheduleConfig = toml::from_str(&contents).map_err(|e| e.to_string())?;
     Ok(config)
 }
 
-pub fn read_annealing_config(path: &str) -> Result<RawAnnealingConfig, String> {
+pub(super) fn read_annealing_config(path: &str) -> Result<RawAnnealingConfig, String> {
     let contents = fs::read_to_string(path).map_err(|e| e.to_string())?;
     let config: RawAnnealingConfig = toml::from_str(&contents).map_err(|e| e.to_string())?;
     Ok(config)
