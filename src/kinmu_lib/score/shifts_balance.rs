7d8703cfe1e54ce2661684c5fb829c595bf25e79
unsharot <U_N_Sharot@yahoo.co.jp>
2025-02-03 17:29:59 +0900

    Fix counting method of score ShiftsBalance

--- src/kinmu_lib/score/shifts_balance.rs
+++ src/kinmu_lib/score/shifts_balance.rs
@@ -9,11 +9,11 @@ pub(super) fn eval(
     (cond, shift1, shift2, score): &mut (CondWrapper, Shift, Shift, Score),
 ) -> Score {
     let mut sum = 0.0;
-    for day in 0..schedule_config.day.count {
+    for staff in 0..schedule_config.staff.count {
         let mut is_valid = false;
         let mut count1: i32 = 0;
         let mut count2: i32 = 0;
-        for staff in 0..schedule_config.staff.count {
+        for day in 0..schedule_config.day.count {
             if cond.eval(staff, day, schedule_config) {
                 is_valid = true;
                 if schedule[staff][day] == *shift1 {
@@ -31,4 +31,53 @@ pub(super) fn eval(
         }
     }
     sum
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::kinmu_lib::types::Cond;
+
+    use super::*;
+
+    /// 2つのシフトの数が同じ場合
+    #[test]
+    fn test_eq() {
+        let schedule = {
+            use Shift::*;
+            vec![vec![O, H, N, N]]
+        };
+
+        let mut schedule_config: ScheduleConfig = Default::default();
+        schedule_config.day.count = schedule[0].len();
+        schedule_config.staff.count = schedule.len();
+
+        let score = eval(
+            &schedule_config,
+            &schedule,
+            &mut (CondWrapper::new(Cond::Every), Shift::O, Shift::H, 1.0),
+        );
+
+        assert_eq!(0.0, score);
+    }
+
+    /// 2つのシフトの数が違う場合
+    #[test]
+    fn test_neq() {
+        let schedule = {
+            use Shift::*;
+            vec![vec![O, H, N, O]]
+        };
+
+        let mut schedule_config: ScheduleConfig = Default::default();
+        schedule_config.day.count = schedule[0].len();
+        schedule_config.staff.count = schedule.len();
+
+        let score = eval(
+            &schedule_config,
+            &schedule,
+            &mut (CondWrapper::new(Cond::Every), Shift::O, Shift::H, 1.0),
+        );
+
+        assert_eq!(1.0, score);
+    }
 }
