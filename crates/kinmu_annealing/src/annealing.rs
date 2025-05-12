2d8e861ca25773162cd15eb9605a1bdc4e4f4deb
unsharot <U_N_Sharot@yahoo.co.jp>
2025-05-12 18:48:16 +0900

    Clean codes and comments of annealing

--- crates/kinmu_annealing/src/annealing.rs
+++ crates/kinmu_annealing/src/annealing.rs
@@ -2,6 +2,8 @@ use rand::Rng;
 
 /// 焼きなましの実行
 /// 一般化されたもの
+/// 
+/// スコアが低くなるようにモデルを最適化する。
 ///
 /// # 例
 ///
@@ -36,7 +38,7 @@ use rand::Rng;
 pub fn run<M, S, U, E, T, P, R>(
     initial_score: S,
     initial_model: &M,
-    loop_count: u32,
+    step_count: u32,
     mut update: U,
     mut eval: E,
     temp_max: f32,
@@ -55,27 +57,26 @@ where
     R: Rng,
 {
     let mut best_model = initial_model.clone();
-    let mut current_model = initial_model.clone();
-
     let mut best_score = initial_score;
+
+    let mut current_model = initial_model.clone();
     let mut current_score = initial_score;
 
     let mut temp;
 
-    for loop_value in 0..loop_count {
+    for loop_value in 0..step_count {
         let next_model = update(&current_model, rng);
         let next_score = eval(&next_model);
-        temp = temp_func(temp_max, temp_min, loop_count, loop_value);
 
-        if next_score <= best_score {
-            best_model = next_model.clone();
+        temp = temp_func(temp_max, temp_min, step_count, loop_value);
+
+        if rng.gen::<f32>() < prob_func(current_score, next_score, temp) {
+            current_model = next_model.clone();
+            current_score = next_score;
+        }
+        if next_score < best_score {
+            best_model = next_model;
             best_score = next_score;
-
-            current_model = next_model;
-            current_score = next_score;
-        } else if rng.gen::<f32>() < prob_func(current_score, next_score, temp) {
-            current_model = next_model;
-            current_score = next_score;
         }
     }
 
@@ -84,13 +85,14 @@ where
 
 /// 標準の温度関数
 /// 与えられた最大温度と最低温度から、ステップに対して線形な温度を返す
-pub fn basic_temp_func(temp_max: f32, temp_min: f32, loop_end: u32, loop_now: u32) -> f32 {
-    let r: f32 = (loop_end - loop_now) as f32 / loop_end as f32;
+pub fn basic_temp_func(temp_max: f32, temp_min: f32, step_end: u32, step_now: u32) -> f32 {
+    let r: f32 = (step_end - step_now) as f32 / step_end as f32;
     temp_max - ((temp_max - temp_min) * r)
 }
 
 /// 標準の確率関数
 /// 前後のスコアと温度から、スコアが悪化した場合に更新する確率を返す
+/// スコアが改善した場合、常に1を越える値を返す
 pub fn basic_prob_func(score_now: f32, score_next: f32, temp: f32) -> f32 {
     ((score_now - score_next) / temp).exp()
 }
