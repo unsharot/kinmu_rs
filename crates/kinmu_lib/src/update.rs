37582a0fe2b0f40a424037ad002a64c36c9af347
unsharot <132771408+unsharot@users.noreply.github.com>
2025-06-30 23:03:11 +0900

    Add update func `update_range`

--- crates/kinmu_lib/src/update.rs
+++ crates/kinmu_lib/src/update.rs
@@ -33,6 +33,15 @@ impl Update<StdScoreProp, Shift, ShiftState, DayState> for StdUpdate {
             "update_iak_safe" => Ok(Box::new(move |schedule, rng| {
                 update_iak_safe(schedule_config, schedule_state, schedule, rng)
             })),
+            "update_range" => Ok(Box::new(move |schedule, rng| {
+                update_range(schedule_config, schedule_state, schedule, rng)
+            })),
+            "update_range_or_noh_repeat" => Ok(Box::new(move |schedule, rng| {
+                update_range_or_noh_repeat(schedule_config, schedule_state, schedule, rng)
+            })),
+            "update_range_or_iaknoh_repeat" => Ok(Box::new(move |schedule, rng| {
+                update_range_or_iaknoh_repeat(schedule_config, schedule_state, schedule, rng)
+            })),
             _ => Err(anyhow::anyhow!(
                 "Failed to generate update function {}",
                 name
@@ -276,4 +285,74 @@ fn update_iak_safe<R: Rng>(
         }
     }
     new_schedule
+}
+
+/// ランダムなスタッフ同士の連続する1~3日分の範囲を入れ替える
+fn update_range<R: Rng>(
+    schedule_config: &ScheduleConfig,
+    schedule_state: &ScheduleState,
+    schedule: &Schedule,
+    rng: &mut R,
+) -> Schedule {
+    let mut new_schedule = schedule.clone();
+    let range_size = rng.gen_range(1..3);
+
+    let staff1 = rng.gen_range(0..schedule_config.staff.count);
+    let staff2 = rng.gen_range(0..schedule_config.staff.count);
+
+    let day1_begin = rng.gen_range(0..schedule_config.day.count - range_size + 1);
+    let day2_begin = rng.gen_range(0..schedule_config.day.count - range_size + 1);
+
+    let mut cancel = false;
+
+    for dd in 0..range_size {
+        // 交換
+        new_schedule[staff1][day1_begin + dd] = schedule[staff2][day2_begin + dd];
+        new_schedule[staff2][day2_begin + dd] = schedule[staff1][day1_begin + dd];
+
+        // Absoluteかつ一致しないなら
+        let abs1 = schedule_state[staff1][day1_begin + dd] == ShiftState::Absolute;
+        let abs2 = schedule_state[staff2][day2_begin + dd] == ShiftState::Absolute;
+        let same1 = new_schedule[staff1][day1_begin + dd] == schedule[staff1][day1_begin + dd];
+        let same2 = new_schedule[staff1][day1_begin + dd] == schedule[staff1][day1_begin + dd];
+        if (abs1 && !same1) || (abs2 && !same2) {
+            cancel = true;
+        }
+    }
+
+    if cancel {
+        schedule.clone()
+    } else {
+        new_schedule
+    }
+}
+
+/// rangeとnoh_repeatをランダムに等確率で実行
+fn update_range_or_noh_repeat<R: Rng>(
+    schedule_config: &ScheduleConfig,
+    schedule_state: &ScheduleState,
+    schedule: &Schedule,
+    rng: &mut R,
+) -> Schedule {
+    let r = rng.gen_range(0..2);
+    if r == 0 {
+        update_range(schedule_config, schedule_state, schedule, rng)
+    } else {
+        update_noh_repeat(schedule_config, schedule_state, schedule, rng)
+    }
+}
+
+/// rangeとiaknoh_repeatをランダムに等確率で実行
+fn update_range_or_iaknoh_repeat<R: Rng>(
+    schedule_config: &ScheduleConfig,
+    schedule_state: &ScheduleState,
+    schedule: &Schedule,
+    rng: &mut R,
+) -> Schedule {
+    let r = rng.gen_range(0..2);
+    if r == 0 {
+        update_range(schedule_config, schedule_state, schedule, rng)
+    } else {
+        update_iaknoh_repeat(schedule_config, schedule_state, schedule, rng)
+    }
 }
